7b2fd16179c0cd22be681819b8bba5bc1c048c7e
yuralala8 <yc1435@nyu.edu>
2017-06-20 21:06:12 -0400

    key for min value commit

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_key = nil
+    min_val = nil
+    
+    name_hash.each do |name, value|
+        if min_val == nil
+            min_key =name
+            min_val = value
+        end
+        if value < min_val
+            min_key = name
+            min_val = value
+            end
+        end
+    min_key
 end
+
